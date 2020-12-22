c13e4872fbf20cd9a60841df5b40a2f5fd448317
M3RCURRRY <49792824+M3RCURRRY@users.noreply.github.com>
2020-12-22 21:52:09 +0300

    Update belt_def.h

--- belt_def.h
+++ belt_def.h
@@ -1,5 +1,27 @@
 #include "belt_utils.h"
 
+/*
+ Для каждого режима (их всего представлено три проекте :
+ sswap - режим простой замены блоков
+ cblock - режим объединения блоков
+ gamma - режим гаммирования с обратной связью)
+ объявлены функции, описание которых находится в belt_sswap.c
+ belt_cblock.c и belt_gamma.c соответственно.
+
+ Функции с суффиксом init инициализируют алгоритм шифрования в
+ заданном режиме (задают начальное состояние, инициализируют
+ структуру этого состояния).
+
+ Функции с суффиксом EncBlock/DecBlock - шифруют/дешифруют один блок.
+ Используются в функциях Encrypt/Decrypt, которые работают с цельным
+ сообщением.
+
+ Функции с суффиксами Encrypt/Decrypt имеют тип int с целью возможности
+ возврата кода ошибки, которая могла произошла в ходе работы алгоритма,
+ либо при некорректном поданом сообщении на входе. В случае корректного
+ окончания работы функции возвращается 0.
+*/
+
 void cblock_init(void*, const uchar[], size_t, const uchar[16]);
 void cblock_EncBlock(void*, size_t, void*);
 void cblock_DecBlock(void*, size_t, void*);
@@ -17,5 +39,3 @@ void gamma_EncBlock(void*, size_t, void*);
 void gamma_DecBlock(void*, size_t, void*);
 int gamma_Encrypt(void*, const void*, size_t, const uchar[], size_t, const uchar[16]);
 int gamma_Decrypt(void*, const void*, size_t, const uchar[], size_t, const uchar[16]);
-
-//cblock_Encrypt
